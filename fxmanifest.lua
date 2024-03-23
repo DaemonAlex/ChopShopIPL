fdfc53a459404b4479cf87a60b31b75dd784807a
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-23 17:24:33 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'DeamonAlex'
+description 'IPLs for CS'
+version '1.0.0'
+
+shared_scripts {
+    'Config.lua'
+}
+
+client_scripts {
+	"main.lua"
+}
+
+dependencies {
+    '/server:7257',
+    '/gameBuild:3095',
+}
